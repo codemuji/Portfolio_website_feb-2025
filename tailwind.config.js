3555eb0b853a9576ba3cd74f54368c1de5052a4f
codemuji <rajatpradhan011@gmail.com>
2025-02-09 12:03:41 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,ts,jsx,tsx}"], // Adjust paths based on your project structure
+    theme: {
+      extend: {}, // Extend Tailwind’s default theme here
+    },
+    plugins: [], // Add Tailwind plugins here
+  };
